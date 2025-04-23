ce58c5129c5341cb0de2c661d27e26d6362c09ba
maurin.stutz <maurin.stutz@orca.ch>
2025-04-23 15:17:07 +0200

    #4 #5 Setup CakeDC Users plugin with registration, login and logout
    - Load plugin
    - Add some of the built-in tables through an application-internal migration, instead of the plugin built-in ones
    - Add empty application-specific Users plugin config
    - Add default users plugin permissions

---
+++ config/permissions.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Permission configuration for CakeDC Users plugin
+ *
+ * @see https://github.com/CakeDC/users/blob/15.next-cake5/config/permissions.php
+ */
+
+use Cake\Http\ServerRequest;
+use Cake\Utility\Hash;
+use CakeDC\Users\Model\Table\UsersTable;
+
+/*
+ * IMPORTANT:
+ * This is an example configuration file. Copy this file into your config directory and edit to
+ * set up your app permissions.
+ *
+ * This is a quick roles-permissions implementation
+ * Rules are evaluated top-down, first matching rule will apply
+ * Each line define
+ *      [
+ *          'role' => 'role' | ['roles'] | '*'
+ *          'prefix' => 'Prefix' | , (default = null)
+ *          'plugin' => 'Plugin' | , (default = null)
+ *          'controller' => 'Controller' | ['Controllers'] | '*',
+ *          'action' => 'action' | ['actions'] | '*',
+ *          'allowed' => true | false | callback (default = true)
+ *      ]
+ * You could use '*' to match anything
+ * 'allowed' will be considered true if not defined. It allows a callable to manage complex
+ * permissions, like this
+ * 'allowed' => function (array $user, $role, Request $request) {}
+ *
+ * Example, using allowed callable to define permissions only for the owner of the Posts to edit/delete
+ *
+ * (remember to add the 'uses' at the top of the permissions.php file for Hash, TableRegistry and Request
+   [
+        'role' => ['user'],
+        'controller' => ['Posts'],
+        'action' => ['edit', 'delete'],
+        'allowed' => function(array $user, $role, Request $request) {
+            $postId = Hash::get($request->params, 'pass.0');
+            $post = TableRegistry::getTableLocator()->get('Posts')->get($postId);
+            $userId = Hash::get($user, 'id');
+            if (!empty($post->user_id) && !empty($userId)) {
+                return $post->user_id === $userId;
+            }
+            return false;
+        }
+    ],
+ */
+
+return [
+    'CakeDC/Auth.permissions' => [
+        // All bypasses for CakeDC Users plugin
+        [
+            'prefix' => false,
+            'plugin' => 'CakeDC/Users',
+            'controller' => 'Users',
+            'action' => [
+                // LoginTrait
+                'login',
+                'logout',
+                'socialEmail',
+                'verify',
+                // RegisterTrait
+                'register',
+                'validateEmail',
+                // PasswordManagementTrait used in RegisterTrait
+                'changePassword',
+                'resetPassword',
+                'requestResetPassword',
+                // UserValidationTrait used in PasswordManagementTrait
+                'resendTokenValidation',
+                'linkSocial',
+                //Webauthn2fa actions
+                'webauthn2fa',
+                'webauthn2faRegister',
+                'webauthn2faRegisterOptions',
+                'webauthn2faAuthenticate',
+                'webauthn2faAuthenticateOptions',
+                'requestLoginLink',
+                'sendLoginLink',
+                'singleTokenLogin',
+            ],
+            'bypassAuth' => true,
+        ],
+        // Admin role allowed to all the things
+        [
+            'role' => UsersTable::ROLE_ADMIN,
+            'prefix' => '*',
+            'extension' => '*',
+            'plugin' => '*',
+            'controller' => '*',
+            'action' => '*',
+        ],
+        // Specific actions allowed for the all roles in Users plugin
+        [
+            'role' => '*',
+            'plugin' => 'CakeDC/Users',
+            'controller' => 'Users',
+            'action' => [
+                'profile',
+                'logout',
+                'linkSocial',
+                'callbackLinkSocial',
+            ],
+        ],
+        [
+            'role' => '*',
+            'plugin' => 'CakeDC/Users',
+            'controller' => 'Users',
+            'action' => 'resetOneTimePasswordAuthenticator',
+            'allowed' => function (array $user, $role, ServerRequest $request) {
+                $userId = Hash::get($request->getAttribute('params'), 'pass.0');
+                if (!empty($userId) && !empty($user)) {
+                    return $userId === $user['id'];
+                }
+
+                return false;
+            },
+        ],
+        // All roles allowed to Pages/display
+        [
+            'role' => '*',
+            'controller' => 'Pages',
+            'action' => 'display',
+        ],
+        // Bypass auth for DebugKit
+        [
+            'role' => '*',
+            'plugin' => 'DebugKit',
+            'controller' => '*',
+            'action' => '*',
+            'bypassAuth' => true,
+        ],
+    ],
+];
